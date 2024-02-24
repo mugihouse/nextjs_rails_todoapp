5b7fdd9cc69ba4924706d81b35dcc58478b87fca
mugihouse <mugi.stnk100122@gmail.com>
2024-02-24 10:53:30 +0900

    Add: todoデータの更新api追加

--- todo-backend/app/controllers/todos_controller.rb
+++ todo-backend/app/controllers/todos_controller.rb
@@ -24,6 +24,16 @@ class TodosController < ApplicationController
     end
   end
 
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
   def todo_params
     params.require(:todo).permit(:title, :content)
   end
